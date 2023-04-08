ecec67e77ac176114e1782698851ebf3a850d363
Hans Kratz <hans@appfour.com>
2023-04-08 16:27:17 +0200

    add qualified tests

--- tests/string_tests.rs
+++ tests/string_tests.rs
@@ -33,3 +33,40 @@ fn test_swriteln_empty() {
     swriteln!(buf,);
     assert_eq!(buf, "\n");
 }
+
+mod qualified {
+    use swrite::SWrite;
+
+    #[test]
+    fn test_swrite() {
+        let mut buf = String::new();
+        swrite::swrite!(buf, "Hello, {}!", "world");
+        assert_eq!(buf, "Hello, world!");
+    }
+
+    #[test]
+    fn test_swriteln() {
+        let mut buf = String::new();
+        swrite::swriteln!(buf, "Hello, {}!", "world");
+        assert_eq!(buf, "Hello, world!\n");
+    }
+
+    #[test]
+    fn test_swriteln_appends() {
+        let mut buf = String::new();
+        swrite::swrite!(buf, "Hello, {}", "world");
+        assert_eq!(buf, "Hello, world");
+        swrite::swriteln!(buf, "!");
+        assert_eq!(buf, "Hello, world!\n");
+    }
+
+    #[test]
+    fn test_swriteln_empty() {
+        let mut buf = String::new();
+        swrite::swriteln!(buf);
+        assert_eq!(buf, "\n");
+        let mut buf = String::new();
+        swrite::swriteln!(buf,);
+        assert_eq!(buf, "\n");
+    }
+}
